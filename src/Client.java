8520f211c06b9ebea62d97d1197c55dff70aa91a
Davide-Marchese <6190189@itisrossi.vi.it>
2023-01-25 15:38:32 +0100

    debugging

--- src/Client.java
+++ src/Client.java
@@ -2,12 +2,51 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
+    public static void negozio(BufferedReader read, BufferedReader in, PrintWriter out) {
+        String scelta = "";
+        System.out.println("Benvenuto in eHub");
+        while (true) {
+            System.out.println("Seleziona l'operazione da svolgere:\nVisita\nAcquisto");
+            try {
+                scelta = read.readLine();
+                while (!scelta.equals("Visita") && !scelta.equals("Acquisto")) {
+                    System.out.println("Selezionare l'operazione da svolgere:\nVisita\nAcquisto");
+                    scelta = read.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            out.println(scelta);
+            try {
+                String result = in.readLine();
+                System.out.println(result);
+                if (scelta.equals("Acquisto")) {
+                    String acquisto = "";
+                    boolean isCorrect = false;
+                    do {
+                        acquisto = read.readLine();
+                        isCorrect = true;
+                        try {
+                            Integer.parseInt(acquisto);
+                        } catch (NumberFormatException e) {
+                            isCorrect = false;
+                            System.out.println(result);
+                        }
+                    } while (!isCorrect);
+                    out.println(acquisto);
+                    System.out.println(in.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         Socket server = null;
         BufferedReader in = null;
         PrintWriter out = null;
-        String scelta = "";
         try {
             server = new Socket(InetAddress.getLocalHost(), 10000);
             in = new BufferedReader(new InputStreamReader(server.getInputStream()));
@@ -15,28 +54,6 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        while(true){
-            System.out.println("Benvenuto in eHub, seleziona l'operazione da svolgere:\nVisita\nAcquisto");
-        try {
-            scelta = read.readLine();
-            while (!scelta.equals("Visita") && !scelta.equals("Acquisto")) {
-                System.out.println("Selezionare l'operazione da svolgere:\nVisita\nAcquisto");
-                scelta = read.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        out.println(scelta);
-        try {
-            System.out.println(in.readLine());
-            if (scelta.equals("Acquisto")) {
-                String acquisto = read.readLine();
-                out.println(acquisto);
-                System.out.println(in.readLine());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        }
+        negozio(read, in, out);
     }
 }
